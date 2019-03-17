dde9b91e27d9efb9920c6285abcdf523bd2e258f
jfustos <jacobfustos@ku.edu>
2019-03-17 16:39:46 -0500

    Added data / graph generator for spec2006 benchmarks

---
+++ parse-spec2006.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @bench_names = ('bzip2', 'mcf', 'gobmk', 'hmmer', 'sjeng', 'libquantum', 'h264ref', 'omnetpp', 'astar', 'bwaves', 'gamess', 'milc', 'zeusmp', 'gromacs', 'cactusADM', 'leslie3d', 'namd', 'soplex', 'calculix', 'GemsFDTD', 'tonto', 'lbm', 'sphinx3');
+my @configs = ('Native', 'InvisiSpec', 'Fence', 'SG-Opt-NoS', 'SG-Opt-All', 'SG-Base-NoS', 'SG-Base-All');
+my %configs_display_name = (
+    Native        => 'Native',
+    InvisiSpec    => 'InvisiSpec',
+    Fence         => 'Fence',
+    'SG-Opt'      => 'SG-Opt',
+    'SG-Opt-NoS'  => 'SG-Opt-NoS',
+    'SG-Opt-All'  => 'SG-Opt-All',
+    'SG-Base'     => 'SG-Base',
+    'SG-Base-NoS' => 'SG-Base-NoS',
+    'SG-Base-All' => 'SG-Base-All',
+);
+
+my %all_benches;
+
+foreach my $config (@configs) {
+    foreach my $bench ( @bench_names )
+    {
+        my $bench_path = "artifacts/results/spec2006/${config}/${bench}/stats.txt";
+        
+        open(my $stats_handle, "<", $bench_path) or die "Can't open |$bench_path|: says |$!|";
+        while (<$stats_handle>)
+        {
+            my @spl = split ' ';
+            if (defined($spl[0]) && $spl[0] eq 'sim_seconds') {
+                $all_benches{$config}{$bench}{sim_seconds} = $spl[1];
+                last;
+            }
+        }
+
+        close($stats_handle);
+        die "Did not find sim_seconds in file |${bench_path}|!!\n\n" unless exists $all_benches{$config}{$bench}{sim_seconds};
+    }
+}
+
+if( -e "./artifacts/graphs/spec2006" )
+{
+    system "rm -rf ./artifacts/graphs/spec2006";
+}
+
+system "mkdir -p ./artifacts/graphs/spec2006";
+
+my $spec_perf_path_dat = "artifacts/graphs/spec2006/spec2006-perf.dat";
+open(my $spec_perf_handle_dat, ">", $spec_perf_path_dat) or die "Can't open > $spec_perf_path_dat: $!";
+
+printf $spec_perf_handle_dat "scheme\t";
+foreach my $config ( @configs )
+{
+    printf $spec_perf_handle_dat $configs_display_name{$config} . "\t";
+    $all_benches{$config}{norm_time} = 0;
+}
+printf $spec_perf_handle_dat "\n";
+
+foreach my $bench (@bench_names)
+{
+    printf $spec_perf_handle_dat "$bench\t";
+    foreach my $config ( @configs )
+    {
+        my $norm_time = $all_benches{$config}{$bench}{sim_seconds} / $all_benches{Native}{$bench}{sim_seconds};
+        printf $spec_perf_handle_dat "%.3f\t", $norm_time;
+        
+        $all_benches{$config}{norm_time} = $norm_time + $all_benches{$config}{norm_time};
+    }
+    printf $spec_perf_handle_dat "\n";
+}
+
+printf $spec_perf_handle_dat "average\t";
+foreach my $config ( @configs )
+{
+    printf $spec_perf_handle_dat "%.3f\t", $all_benches{$config}{norm_time} / @bench_names;
+}
+
+system "gnuplot scripts/spec2006-perf.gnu > artifacts/graphs/spec2006/spec2006-perf.pdf";
+
+exit 0;
---
+++ scripts/spec2006-perf.gnu
@@ -0,0 +1,48 @@
+set terminal pdfcairo font "Gill Sans,16" size 14.0,3.5 linewidth 2 rounded #fontscale 1.0
+
+set style line 80 lt rgb "#808080"
+# Line style for grid
+set style line 81 lt 0  # dashed
+set style line 81 lt rgb "#808080"  # grey
+set style line 82 lt 0  # dashed
+set style line 82 lt rgb "#000000"  # grey
+
+set grid back linestyle 81
+set border 1 back linestyle 80 
+set xtics nomirror
+set ytics nomirror
+
+#set style line 1 lt rgb "#A00000" lw 2 pt 1
+#set style line 2 lt rgb "#00A000" lw 2 pt 6
+#set style line 3 lt rgb "#5060D0" lw 2 pt 2
+#set style line 4 lt rgb "#F25900" lw 2 pt 9
+
+set style line 1 lt rgb "#000000" lw 2 pt 1
+set style line 2 lt rgb "#000040" lw 2 pt 6
+set style line 3 lt rgb "#000080" lw 2 pt 2
+set style line 4 lt rgb "#0000a0" lw 2 pt 9
+
+set yrange [0:3.5]
+
+set xtics nomirror rotate by -40 scale 0 font ",18"
+set ylabel "Normalized Execution Time" offset 2
+set style data histogram
+set key left height -0.5 
+set key samplen 1 width 2
+set key autotitle columnhead
+set boxwidth 1
+set arrow from -1,1 to 4,1 nohead lt 2
+# set arrow from 0.6,0 to 0.6,1.4 nohead lt 1
+
+# set label "4.99" at 1.45,5 font ",16" front
+set style fill pattern
+plot  "artifacts/graphs/spec2006/spec2006-perf.dat" using 2:xticlabel(1) fs pattern 0 lt -1,\
+       ''          using 3:xticlabel(1) fs pattern 1 lt -1,\
+       ''          using 4:xticlabel(1) fs pattern 2 lt -1,\
+       ''          using 5:xticlabel(1) fs pattern 4 lt -1,\
+       ''          using 6:xticlabel(1) fs pattern 3 lt -1,\
+       ''          using 7:xticlabel(1) fs pattern 5 lt -1,\
+       ''          using 8:xticlabel(1) fs pattern 6 lt -1
+
+# ''          using 5:xticlabel(1) fs pattern 3 lt -1,\   
+# ''          using 9:xticlabel(1) fs pattern 7 lt -1
